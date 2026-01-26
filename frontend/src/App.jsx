86cff2dd02610ed45d29218d278c1569493e5749
BtJ <ymawro@gmail.com>
2026-01-26 18:00:47 +0900

    feat: add todo app react-hook form ver

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import TodoList from "./component/TodoList";
+import TodoListRHF from "./component/TodoListRHF";
 
 function App() {
   return (
     <>
       <main>
         <h1>Task List</h1>
-        <TodoList></TodoList>
+        {/* <TodoList></TodoList> */}
+        <TodoListRHF></TodoListRHF>
       </main>
     </>
   );
