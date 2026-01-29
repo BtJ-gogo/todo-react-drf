de5a5340a40a9e14abd4cf94aa31ff4441ec98d8
BtJ <ymawro@gmail.com>
2026-01-29 16:53:53 +0900

    feat: add refresh

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,14 +1,53 @@
-const fetchTasks = async (url, options={}, nav, authHeaders) => {
+const fetchTasks = async (url, options={}, navigate, authHeaders) => {
+    const logout = () => {
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        navigate("/login");
+    }
     const response = await fetch(url, {
         ...options,
         headers: authHeaders(),
     });
 
     if (response.status === 401) {
-        localStorage.removeItem("access");
-        localStorage.removeItem("refresh");
-        nav("/login");
-        throw new Error("Unauthorized");
+        const refreshToken = localStorage.getItem("refresh");
+        if (!refreshToken) {
+            logout();
+            throw new Error("Unauthorized");
+        }
+        
+        try {
+            const refreshResponse = await fetch("http://localhost:8000/api/token/refresh/", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ refresh: refreshToken }),
+            });
+
+            if (!refreshResponse.ok) {
+                logout();
+                throw new Error("Unauthorized");
+            }
+
+            const refreshData = await refreshResponse.json();
+            localStorage.setItem("access", refreshData.access);
+            const newAuthHeaders = {
+                ...authHeaders(),
+                Authorization: `Bearer ${refreshData.access}`,
+            };
+
+            const retryResponse = await fetch(url, {
+                ...options,
+                headers: newAuthHeaders,
+            });
+            return retryResponse;
+
+        } catch (error) {
+            localStorage.removeItem("access");
+            localStorage.removeItem("refresh");
+            navigate("/login");
+            throw new Error(error.message);
+        }
+               
     }
 
     if (!response.ok) {
