b525b4a1c768f3c9c734a88f5a40d9a09d998bc5
BtJ <ymawro@gmail.com>
2026-01-29 22:07:25 +0900

    refactor: add UserFiteredMixin and use on views

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -8,20 +8,19 @@ from .serializers import TodoSerializer
 from .models import Todo
 
 
-class TaskListCreateAPIView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = TodoSerializer
-
+class UserFilteredMixin:
     def get_queryset(self):
         return Todo.objects.filter(user=self.request.user)
+
+
+class TaskListCreateAPIView(UserFilteredMixin, ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TodoSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
-class TaskUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+class TaskUpdateDeleteView(UserFilteredMixin, RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TodoSerializer
-
-    def get_queryset(self):
-        return Todo.objects.filter(user=self.request.user)
